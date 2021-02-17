a71c7711a3c7cd2ed88ee066db4b3074e14253b2
Nihar Srivastava <nsrivastava999@gmail.com>
2021-02-17 11:10:34 +0530

    trees que added

--- 2.Trees/94. Binary Tree Inorder Traversal.cpp
+++ 2.Trees/94. Binary Tree Inorder Traversal.cpp
@@ -11,16 +11,35 @@
  */
 class Solution {
 public:
-    void helper(TreeNode*root,vector<int>&v){
-        if(root==NULL)
-            return;
-        helper(root->left,v);
-        v.push_back(root->val);
-        helper(root->right,v);
-    }
+    // void helper(TreeNode*root,vector<int>&v){
+    //     if(root==NULL)
+    //         return;
+    //     helper(root->left,v);
+    //     v.push_back(root->val);
+    //     helper(root->right,v);
+    // }
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     vector<int>v;
+    //     helper(root,v);
+    //     return v;
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>v;
-        helper(root,v);
-        return v;
+        vector<int>res;
+        if(root==NULL)
+            return res;
+        stack<TreeNode*>s;
+        // s.push(root);
+        // root=root->left;
+        while(root || !s.empty()){
+            while(root){
+                s.push(root);
+                root=root->left;
+            }
+            root = s.top();
+            s.pop();
+            res.push_back(root->val);
+            root=root->right;
+        }
+        return res;
     }
 };
