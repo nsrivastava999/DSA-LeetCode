03f4edb51f780b34ee1d2b2327bdb93c7d7f1d8e
Nihar Srivastava <nsrivastava999@gmail.com>
2021-02-18 22:00:13 +0530

    109 added

---
+++ 2.Trees/109. Convert Sorted List to Binary Search Tree.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        // if(head==NULL)
+        //     return NULL;
+        // ListNode*slow=head;
+        // ListNode*fast=head;
+        // while(fast && fast->next){
+        //     slow=slow->next;
+        //     fast=fast->next->next;
+        // }
+        // if(head==slow)
+        //     return new TreeNode(slow->val);
+        // TreeNode*root = new TreeNode(slow->val);
+        // root->left=sortedListToBST(head);
+        // root->right=sortedListToBST(slow->next);
+        // return root;
+        if (!head) 
+            return NULL;
+        if (!head->next) 
+            return new TreeNode(head->val);
+        auto fast = head->next, slow = head;
+        while (fast->next && fast->next->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        auto mid = slow->next;
+        slow->next = NULL;
+        auto root = new TreeNode(mid->val);
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(mid->next);
+        return root;
+    }
+};
